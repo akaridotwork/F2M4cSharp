45e140303d605123091905d1a31aa7f21e708dbe
JahSnchs <missbehavejah@gmail.com>
2022-06-20 12:54:24 +0200

    added files

---
+++ cSharp2Opdrachten/FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileDirOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo userDir = new DirectoryInfo("/Users/jah.sanches/Desktop/Ma/Periode3");
+
+            foreach (var i in userDir.GetFiles())
+            {
+                Console.WriteLine(i.Name);
+
+            }
+
+            
+
+
+
+        }
+    }
+}
